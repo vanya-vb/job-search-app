48689edbecc72103238147b79f011ddf53b69d1c
vanya-vb <vbozadzhieva29@gmail.com>
2025-08-28 19:24:46 +0300

    add react icons

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaMapMarker } from 'react-icons/fa'
 
 export default function JobListing({ job }) {
     const [fullDescription, setFullDescription] = useState(false);
@@ -34,7 +35,7 @@ export default function JobListing({ job }) {
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
-                        <i className="fa-solid fa-location-dot text-lg"></i>
+                        <FaMapMarker className="inline text-lg mb-1 mr-1" />
                         {job.location}
                     </div>
                     <a
