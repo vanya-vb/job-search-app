3a448e61893cbaa6587ce5bbed80a5e2830813a5
vanya-vb <vbozadzhieva29@gmail.com>
2025-08-28 15:51:19 +0300

    extract view all jobs component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import Navbar from "./components/Navbar";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 export default function App() {
   return (
@@ -10,14 +11,7 @@ export default function App() {
       <Hero />
       <HomeCards />
       <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-        >
-      </section>
+      <ViewAllJobs />
     </>
   )
 }
