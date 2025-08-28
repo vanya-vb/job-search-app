69883128f8b733d5c0125ee80c8c86782ed477f3
vanya-vb <vbozadzhieva29@gmail.com>
2025-08-28 19:56:06 +0300

    add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<MainLayout />} >
           <Route index element={<HomePage />} />
           <Route path="/jobs" element={<JobsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </>
