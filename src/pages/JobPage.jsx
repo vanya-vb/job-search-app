3c4e79384a65355a5a0fef66d727f3abb425630f
vanya-vb <vbozadzhieva29@gmail.com>
2025-08-29 14:09:42 +0300

    fix single job page cols

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -39,7 +39,7 @@ export default function JobPage() {
 
             <section className="bg-indigo-50">
                 <div className="container m-auto py-10 px-6">
-                    <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                    <div className="grid grid-cols-1 md:grid-cols-[7fr_3fr] w-full gap-6">
                         <main>
                             <div
                                 className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
